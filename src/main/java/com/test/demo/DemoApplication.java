042cd706e39066d465183531c133bc48c85b45b9
dummyforjenkins <46932953+dummyforjenkins@users.noreply.github.com>
2019-01-23 17:17:21 +0100

    Update DemoApplication.java

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-#i was here
-
-
 @SpringBootApplication
 @RestController
 public class DemoApplication {
