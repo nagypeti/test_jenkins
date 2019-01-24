63dd3514bd26c800e6aa119564113537f6080fcb
dummyforjenkins <46932953+dummyforjenkins@users.noreply.github.com>
2019-01-24 10:07:48 +0100

    run tests

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+not working 
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
